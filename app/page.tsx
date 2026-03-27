8566fb727415c19ee87eb3f4b58824d9f5a2eeda
MatthewHightech <mattsmithwebdev@gmail.com>
2026-03-26 20:58:18 -0700

    Spelling errors

--- app/page.tsx
+++ app/page.tsx
@@ -94,7 +94,7 @@ export default function Home() {
               </p>
               <p>
                 I value ownership, accountability and trust. I value measuring twice and cutting once. I value failing fast. I value calculated
-                risks. I value simplicity as a default and complexity when necessary. I value questioning everything, knowing when to make a descision and maintaining the ability to adapt.
+                risks. I value simplicity as a default and complexity when necessary. I value questioning everything, knowing when to make a decision and maintaining the ability to adapt.
               </p>
             </div>
         </div>
